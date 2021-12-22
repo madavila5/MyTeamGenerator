e194f7faeb0e5f4caf5f35cc884cbf35f00aa8e4
Miguel Davila <migueldavila@Miguels-Mac-mini.local>
2021-12-21 22:04:48 -0800

    update to template manager structure

---
+++ index.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+const employee = require('./lib/employee');
+const manager = require('./lib/manager');
+const engineer = require('./lib/engineer');
+const intern = require('./lib/intern');
--- src/template.js
+++ src/template.js
@@ -1,7 +1,28 @@
 const displayManagers = managersArr =>{
     if(!managersArr){
-
+        return '';
     }
+    return `
+        ${managersArr
+        .map(({name, id, email, officeNumber})=> {
+            return `
+            <div class = 'col-lg-4 col-md-4 col-sm-6 pt-4'>
+            <div class = 'card'>
+            <div class="card-header">
+            <h4 class="card-title">${name}</h4>
+            <i class="fas fa-briefcase fa-lg"></i>
+            <h6 class="card-text">Manager</h6>
+        </div>
+        <div class="card-body">
+            <p class="card-text mb-0">Employee ID: ${id}</p>
+            <p class="link">Email: <a href="mailto:${email}">${email}</a></p>
+            <p class="card-text">Office: ${officeNumber}</p>
+        </div>
+        </div>
+    </div>`;
+            }).join('')
+        }
+        `;
 }
 
 const displayEngineers = engineerArr => {
@@ -12,7 +33,7 @@ const displayEngineers = engineerArr => {
 
 const displayInterns = internsArr => {
     if (!internsArr){
-        
+
     }
 }
 
