551d0d394164e9dd7960325faf1958fee2c0b6d3
Miguel Davila <migueldavila@Miguels-Mac-mini.local>
2021-12-21 22:35:31 -0800

    Update to file structure with inputs

--- index.js
+++ index.js
@@ -5,3 +5,72 @@ const employee = require('./lib/employee');
 const manager = require('./lib/manager');
 const engineer = require('./lib/engineer');
 const intern = require('./lib/intern');
+const template = require('./src/template');
+
+var employeePrompt = [
+    {
+        type: 'input',
+        name: 'officeNumber',
+        message: 'Enter Manager Office Number',
+    },
+    {
+        type: 'input',
+        name: 'username',
+        message: 'Enter Engineer GitHub Username',
+    },
+    {
+        type: 'input',
+        name: 'school',
+        messagel: "Enter Intern's School",
+    }
+]
+
+class team {
+    constructor(){
+        this.employee;
+        this.managers = [];
+        this.engineers = [];
+        this.interns = [];
+    }
+}
+
+initializePrompts(){
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'role',
+            message: 'Select the Employee Role:',
+            choices: ['Manager', 'Engineer', 'Intern'],
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Enter Employee Name',
+            validate: (nameInput) => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log ('Please Enter Employee Name');
+                    return false;
+                }
+            },
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Enter Employee ID',
+            validate: (idInput) => {
+                if (idInput) => {
+                    return true;
+                } else {
+                    console.log('Please Enter Employee ID');
+                    return false;
+                }
+            },
+        },
+    ])
+}
+
+
+
+new team().initializePrompts();
