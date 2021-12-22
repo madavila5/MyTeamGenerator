577d2ae65b41c3fcb583de8dbaa11b82a3786868
Miguel Davila <migueldavila@Miguels-Mac-mini.local>
2021-12-21 20:30:21 -0800

    Template structure set up

---
+++ src/template.js
@@ -0,0 +1,19 @@
+const displayManagers = managersArr =>{
+
+}
+
+const displayEngineers = engineerArr => {
+
+}
+
+const displayInterns = internsArr => {
+
+}
+
+module.exports = (managerArr, engineersArr, internsArr) => {
+    return `
+    
+    
+    
+    `;
+}
