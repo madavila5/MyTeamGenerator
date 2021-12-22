e655f861ffcfabaff59960e212c3dbea5f1c1625
Miguel Davila <migueldavila@Miguels-Mac-mini.local>
2021-12-21 21:45:33 -0800

    Adding to template and index structure

--- src/template.js
+++ src/template.js
@@ -1,13 +1,19 @@
 const displayManagers = managersArr =>{
+    if(!managersArr){
 
+    }
 }
 
 const displayEngineers = engineerArr => {
+    if (!engineerArr){
 
+    }
 }
 
 const displayInterns = internsArr => {
-
+    if (!internsArr){
+        
+    }
 }
 
 module.exports = (managerArr, engineersArr, internsArr) => {
